a4d192eac86b3472960a6aa5f56563d79ab0a51b
Vit <nnphucthinh@gmail.com>
2019-11-09 09:02:42 +0700

    Initial New Folder

---
+++ hello.py
@@ -0,0 +1,12 @@
+'''
+Created on 11/9/2019
+
+@author: Nguyễn Phúc Thịnh
+'''
+
+# xuat ra man hinh chu HELLO
+print('**    **  ******  **      **      *******')
+print('**    **  **      **      **      **   **')
+print('********  ******  **      **      **   **')
+print('**    **  **      **      **      **   **')
+print('**    **  ******  ******* ******* *******')
---
+++ simple.py
@@ -0,0 +1,24 @@
+'''
+Created on 9/11/2019
+
+@author: Nguyễn Phúc Thịnh
+'''
+
+# Giá trị x y cho trước, Học viên có thể thay đổi
+x, y = 10, 5
+
+# In ra giá trị x, y
+print ('x =', x, ', y =', y)
+
+# Tính tổng x + y
+print ("Tổng x+y =", x + y)
+
+# Tính hiệu x - y
+print("Hiệu x-y =", x - y)
+
+# Tính tích x * y
+print("Tích x*y =", x * y)
+
+# Tính thương  x/ y
+print("Thương x/y =", x / y)
+
